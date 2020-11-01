79c13b61e0c370719b455c7f60bbdb1ba044125e
Joselyn Pacherres <joselyn.pacherres@unmsm.edu.pe>
2020-11-01 01:38:53 -0500

    SirwiyQuy#02: Se conecto el index con la base de datos

--- Desarrollo/resources/views/app.blade.php
+++ Desarrollo/resources/views/app.blade.php
@@ -2,12 +2,10 @@
 <html lang="es">
 
 <head>
-    <script src="https://www.paypal.com/sdk/js?client-id=AR8v8RBDfb3jJaWNmQf9llXOAAlSmwaAGB3HrLca3PrV7Z6RNDaZPL6oT16zyp-as52Z347ge7XWeSwM"></script>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kusawasi</title>
-    <link rel="shortcut icon" href="{{ asset('images/logo_circulo_2.ico') }}">
+    <title>SirwiyQuy</title>
+    <link rel="shortcut icon" href="{{ asset('images/Sirwiyquy_b.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
