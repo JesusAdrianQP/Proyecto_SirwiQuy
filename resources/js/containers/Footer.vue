13058991c812952235730aa5d0d81e3806bed874
JesusAdrianQP <64452102+JesusAdrianQP@users.noreply.github.com>
2022-01-22 10:51:09 -0500

    Update Footer.vue

--- resources/js/containers/Footer.vue
+++ resources/js/containers/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="w-full bg-primary-600 text-white text-center h-12 py-1 sm:py-3 inset-x-0 bottom-0">
-    <p class="text-base">&copy; Copyright 2020, SirwiyQuy. Todos los derechos reservados. </p>
+    <p class="text-base">&copy; Copyright 2022, SirwiyQuy. Todos los derechos reservados. </p>
   </footer>
 </template>
